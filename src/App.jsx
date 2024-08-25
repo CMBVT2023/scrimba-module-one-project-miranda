063b484415e738f50b09ba293dc1eb91054cd8ff
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-08-25 15:17:54 -0700

    Added the nav element styling to the page for the final project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
-import React from 'react'
-import reactLogo from './assets/react.svg'
-/*
-Challenge: Starting from scratch, build and render the 
-HTML for our section project. Check the Google slide for 
-what you're trying to build.
+import React from "react";
+import Navbar from "./components/Navbar.jsx";
+import Main from "./components/Main.jsx";
+import './style.css'
 
-We'll be adding styling to it later.
-
-Hints:
-* The React logo is a file in the project tree, so you can
-  access it by using `src="./react-logo.png" in your image
-  element
-* You can also set the `width` attribute of the image element
-  just like in HTML. In the slide, I have it set to 40px
- */
-
-function App() {
-  return (
-    <div>
-      <img width="40px" src={reactLogo}/>
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100k start on Github</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-    </div>
-  )
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
 }
-
-export default App
